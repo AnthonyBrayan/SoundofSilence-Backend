e50a43083823f9a080a0fb72a6d1b07ab9031c5f
Anthony Brayan <brayan.farronan@gmail.com>
2023-09-27 16:42:18 +0200

    Inicio de sesion ok, agrega el password encriptado

--- SoundofSilence/Controllers/UsersController.cs
+++ SoundofSilence/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using Entities;
 using Microsoft.AspNetCore.Mvc;
 using SoundofSilence.IServices;
+using SoundofSilence.Models;
 using SoundofSilence.Services;
 using System.Security.Authentication;
 using System.Web.Http.Cors;
@@ -26,10 +27,9 @@ namespace SoundofSilence.Controllers
         [HttpPost(Name = "InsertUsers")]
         public IActionResult Post([FromBody] Users users)
         {
-
             try
             {
-                var roleName = "Subscribe"; 
+                var roleName = "Subscribe";
                 var roleId = _usersService.GetRoleIdByName(roleName);
 
                 users.Id_rol = roleId;
@@ -43,6 +43,9 @@ namespace SoundofSilence.Controllers
                 }
                 else
                 {
+                    // Hash de la contraseña antes de almacenarla en la base de datos
+                    users.Password = BCrypt.Net.BCrypt.HashPassword(users.Password);
+
                     return Ok(_usersService.InsertUsers(users));
                 }
             }
@@ -51,6 +54,35 @@ namespace SoundofSilence.Controllers
                 return StatusCode(500, $"Error al obtener el ID del rol: {ex.Message}");
             }
         }
+
+        [HttpPost]
+        public IActionResult Login([FromBody] LoginRequestModel loginRequest)
+        {
+            try
+            {
+                var user = _serviceContext.Users.FirstOrDefault(u => u.Email == loginRequest.Email);
+
+                if (user != null && BCrypt.Net.BCrypt.Verify(loginRequest.Password, user.Password))
+                {
+                    return StatusCode(200, "Inicio de sesión exitoso");
+                }
+                else
+                {
+                    return StatusCode(401, "Credenciales incorrectas");
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error al iniciar sesión: {ex.Message}");
+            }
+        }
+
+
+        //private string GenerateAuthToken(int userId)
+        //{
+        //    // Implementa la lógica para generar un token de autenticación, por ejemplo, utilizando JWT.
+        //    // Retorna el token generado.
+        //}
 
     }
 }
