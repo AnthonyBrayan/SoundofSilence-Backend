87a8d48aa71512f154c85bb98e07cf530659d07b
Anthony Brayan <brayan.farronan@gmail.com>
2023-10-07 15:10:00 +0200

    Table create Audiofile Category UserAudio

--- Data/ServiceContext.cs
+++ Data/ServiceContext.cs
@@ -15,6 +15,9 @@ namespace Data
         public ServiceContext(DbContextOptions<ServiceContext> options) : base(options) { }
         public DbSet<Rol> Rol { get; set; }
         public DbSet<Users> Users { get; set; }
+        public DbSet<AudioFiles> AudioFiles { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<UserAudio> UserAudio { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -28,6 +31,20 @@ namespace Data
                 entity.ToTable("Users");
             });
 
+            builder.Entity<AudioFiles>(entity =>
+            {
+                entity.ToTable("AudioFiles");
+            });
+
+            builder.Entity<Category>(entity =>
+            {
+                entity.ToTable("Category");
+            });
+
+            builder.Entity<UserAudio>(entity =>
+            {
+                entity.ToTable("UserAudio");
+            });
         }
 
     }
---
+++ Entities/AudioFiles.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace Entities
+{
+    public class AudioFiles
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id_AudioFiles { get; set; }
+        public string Title { get; set; }
+        public string Video { get; set; }
+        public string Description { get; set; }
+        public string Audio { get; set; }   
+        // Relación con Usuer (muchos a uno)
+        [ForeignKey("Category")]
+        public int Id_category { get; set; }
+
+        [JsonIgnore]
+        public virtual Category Category { get; set; }
+    }
+}
